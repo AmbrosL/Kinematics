23b546d1c9cfb8cc66dddfa3bca208bb00890e74
AmbrosL <AmbrosL@users.noreply.github.com>
2018-06-17 18:42:17 +0200

    start java plugin

---
+++ src/me/bambi/kinematics/Kinematics.java
@@ -0,0 +1,16 @@
+package me.bambi.kinematics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Kinematics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
